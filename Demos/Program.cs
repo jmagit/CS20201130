52bf0b0f7b14edb639b372b919e9990f01ac67bc
Javier <jmartin@grupoloyal.com>
2020-12-02 15:11:53 +0100

    Documentación

--- Demos/Program.cs
+++ Demos/Program.cs
@@ -1,14 +1,29 @@
-﻿using System;
+﻿using otra=ViewNext.Cursos.MiBiblioteca;
+using ViewNext.Cursos.Core;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Demos {
+using static System.Math;
+
+namespace ViewNext.Cursos.Demos {
     delegate void Notificar(object sender, EventArgs ev);
 
+    struct Punto {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Punto(int x, int y) {
+            X = x;
+            Y = y;
+        }
+    }
+    enum Dias: short { Lunes = 1, Martes, Domingo = 0 }
     class MiClase {
         private int count;
+        public otra.OtraClase Demo { get; set; }
 
         public Notificar onChange { get; set; }
 
@@ -51,7 +66,7 @@ namespace Demos {
         public void setCount(int value) { count = value; }
         #endregion
         public double Suma(double a, double b) {
-            return a + b;
+            return Abs(a + b);
         }
     }
     delegate int Dame();
@@ -63,32 +78,56 @@ namespace Demos {
         }
 
         private static void Mio(object sender, EventArgs ev) {
-            Console.WriteLine("El valor es " + ((MiClase)sender).Valor + " unidades");
+            if (sender is MiClase)
+                Console.WriteLine("El valor es " + (sender as MiClase).Valor + " unidades");
             Console.WriteLine("c:\\nada\\otro\\dir");
             Console.WriteLine(@"c:\nada\otro\dir");
             Console.WriteLine($"El valor es {((MiClase)sender).Valor + 1} unidades");
         }
         static void Main(string[] args) {
             var c = new MiClase();
+            OtraClase cc = new OtraClase();
             Dame fn = c.getCount;
+            Dias d = Dias.Lunes;
+            var nombres = Enum.GetNames(typeof(Dias));
+            Punto p1, p2 = new Punto(1,3);
 
+            p1 = p2;
+            p2.X = 3;
+            //cc.GetType()
 #if DEBUG
             Operacion calc = c.Suma;
 #else
             Operacion calc = Divide;
 #endif
             // ...
-            var i = calc(2, 2);
+            var i = calc(2, 2) * 2;
+            short corto = (short)i;
+            int? conNulo = null;
+            if (conNulo.HasValue) {
+                var rslt = i + conNulo.Value;
+            }
 
             if (c.getCount() == 0) {
                 c.setCount(1);
             }
+
+            Debug.WriteLine("Hola mundo");
 
             c.onChange = Mio;
 
             if (c.EstaVacio) {
                 c.Count = 1;
             }
+            string cad;
+            if (c != null && c.Demo != null && c.Demo.Nombre != null)
+                cad = c.Demo.Nombre.ToLower();
+            else
+                cad = null;
+
+            cad = c?.Demo?.Nombre?.ToLower();
+            var cad2 =(cad ?? "Vacio").ToLower();
+            Console.ReadLine();
         }
     }
 }
---
+++ MiBiblioteca/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewNext.Cursos.MiBiblioteca {
+    public class OtraClase {
+        public String Nombre { get; set; }
+
+    }
+}
+
+namespace ViewNext.Cursos.Core {
+    public class OtraClase {
+        public String Nombre { get; set; }
+
+    }
+}
